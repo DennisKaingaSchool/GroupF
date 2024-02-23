e3c0c23798c9038ecd719c2d42c666770fbf6179
Dennis Kainga <denniskainga@gmail.com>
2024-02-23 15:14:08 +0300

    initialized the UI

--- polls/views.py
+++ polls/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render,HttpResponse
 from polls.models import Member
 # Create your views here.
 
-# def home(request):
-#     return HttpResponse("Hellpo world")
-
-
-
 def home(request):
     members = Member.objects.all()
-    return  HttpResponse(members)
+    context = {
+        'members':members
+    }
+    return  render(request,'polls/home.html',context)
+
+
+def members(request):
+    return render(request,'polls/members/index.html')
 
 
 def about(request):
     return HttpResponse("this is about page")
+
