954adc621a2de1e7b590620e0cf8501240618f5f
SNJshaka <snjishaka@gmail.com>
2024-03-11 12:45:19 +0300

    refactored code for readerbility

--- polls/static/polls/js/main.js
+++ polls/static/polls/js/main.js
@@ -1,44 +1,49 @@
+Certainly! I've added comments to explain each section of the code for better clarity:
+
+```javascript
 (function ($) {
     "use strict";
 
     // Spinner
-    var spinner = function () {
+    function hideSpinner() {
+        // Delayed hiding of the spinner element
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
+            $('#spinner').toggleClass('show', false);
         }, 1);
-    };
-    spinner();
-    
-    
+    }
+    hideSpinner();
+
     // Initiate the wowjs
     new WOW().init();
 
-
     // Sticky Navbar
     $(window).scroll(function () {
+        var $stickyTop = $('.sticky-top');
+        // Add shadow and adjust top position when scrolling down
         if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+            $stickyTop.addClass('shadow-sm').css('top', '0px');
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            // Remove shadow and adjust top position when scrolling up
+            $stickyTop.removeClass('shadow-sm').css('top', '-100px');
         }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
     });
 
+    // Back to top button
+    $(window).scroll(function () {
+        var $backToTop = $('.back-to-top');
+        // Show or hide back-to-top button based on scroll position
+        if ($(this).scrollTop() > 300) {
+            $backToTop.fadeIn('slow');
+        } else {
+            $backToTop.fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        // Smooth scroll to the top on button click
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
@@ -46,19 +51,19 @@
         time: 2000
     });
 
-
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
+    var $portfolioContainer = $('.portfolio-container');
+    var portfolioIsotope = $portfolioContainer.isotope({
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
+
     $('#portfolio-flters li').on('click', function () {
+        // Highlight the selected filter and apply filtering
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -68,12 +73,13 @@
         dots: false,
         loop: true,
         nav: true,
-        navText : [
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
 
-    
 })(jQuery);
+```
 
+These comments should provide a clear understanding of each section and make the code more accessible to anyone reading it.
